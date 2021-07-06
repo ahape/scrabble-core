c19f9e039eac5648291cb0390e0f967daa48cb16
Alan Hape <ahape@brightmetrics.com>
2021-07-05 18:37:52 -0700

    Add another playthru test

--- tests/games.spec.ts
+++ tests/games.spec.ts
@@ -5,14 +5,13 @@ import { parseBoard } from "../src/functions/parseboard";
 const game001 = JSON.parse(
     `{"id":"52ce6533-edd6-48a1-abab-5c9e1c254f6c","actions":["NEW_GAME","DRAW NUBELRX","PLAY LUBE H8 H","DRAW VGOIZGY","PLAY VIZE K5 V","DRAW HUEN","PLAY HEX G7 H","DRAW AOP","PLAY PIGGY J6 H","DRAW HTS","PLAY HUNTS D9 H","DRAW VEAI","PLAY HOOVE D9 V","DRAW IBERS","PLAY BERRY N2 V","DRAW NNCR","PLAY NICER A13 H","DRAW NOWI","PLAY SNOW A12 V","DRAW DTEO","PLAY DATE K9 H","DRAW PTJ","PLAY JET M3 H","DRAW ?ECA","PLAY CAsE O7 V","DRAW ST","PLAY PINTS K11 H","DRAW FL?N","PLAY LOAF I12 H","DRAW EEMWO","PLAY WINE A15 H","DRAW EOII","PLAY NITE O1 V","DRAW IFU","PLAY WOOF B11 H","DRAW RQO","PLAY NORE L4 H","DRAW AOE","PLAY TEAM N11 V","DRAW LY","PLAY QAt M13 H","DRAW GRA","PLAY tAG O13 V","DRAW AA","PLAY RAW B9 V","DRAW UA","PLAY LURE I12 V","DRAW DD","PLAY DELAY H15 H","DRAW SAD","PLAY SOD A8 V","DRAW RMLT","PLAY MOTH G4 V","DRAW TKE","PLAY KITE H1 V","DRAW IEI","PLAY RELIMED C4 H","PLAY ALA E3 V","PLAY KI H1 H","PLAY NU F9 V","GAME_OVER 0"],"actionIndex":60,"teams":2}`
 );
+const game002 = JSON.parse(
+    `{"id":"8da5a63a-7f78-43ee-a563-ca579894d91c","teams":2,"actions":["NEW_GAME","DRAW DI?NLEC","PLAY DECLINe B8 H","DRAW OEJIVUO","PLAY JUVIe H4 V","DRAW PMGIEEH","PLAY LIMP E8 V","DRAW AADO","PLAY JADE H4 H","DRAW BEN","PLAY BEEP B11 H","DRAW UGS","PLAY GOOS L1 V","DRAW ORE","PLAY BONER B11 V","DRAW NRAK","PLAY GUNK L1 H","DRAW EQAR","PLAY QUEER G5 H","DRAW ERS","PLAY ARREARS A15 H","DRAW NNRT","PLAY HANGS G11 V","DRAW OEAGDE","PLAY GOOD B5 V","DRAW DEOA","PLAY RODENT J2 V","DRAW WXU","PLAY AX H11 V","DRAW YAIN","PLAY NADE I9 V","DRAW ZW","PLAY WAZED H10 H","DRAW USI?","PLAY YEA C7 V","DRAW ISAE","PLAY WEE K11 H","DRAW HO","PLAY SHOo N11 V","DRAW III","SWAP IIIIU FLTFL","DRAW TITL","PLAY TITI L15 H","PLAY SOFT A12 H","DRAW PIMR","PLAY GULP B5 H","DRAW IIO","PLAY FAIL K9 H","DRAW VCA","PLAY ORCA L3 H","DRAW TUYB","PLAY BOY M8 H","DRAW T","PLAY VIN E13 H","PLAY TULIP E1 V","GAME_OVER 0"],"actionIndex":55}`
+);
+
+const game = new Game();
 
 describe("game001", () => {
-    let game: Game;
-
-    beforeEach(() => {
-        game = new Game();
-    });
-
     it("completes without any errors", () => {
         let errorMessage = "";
 
@@ -92,6 +91,91 @@ describe("game001", () => {
         game.load(game001);
 
         const scores = [270, 308];
+
+        console.assert(_.isEqual(scores, game.status().scores));
+    });
+});
+
+describe("game002", () => {
+    it("completes without any errors", () => {
+        let errorMessage = "";
+
+        try {
+            game.load(game002);
+        } catch (err) {
+            errorMessage = err.message;
+        }
+
+        console.assert(errorMessage === "");
+    });
+
+    it("loads the correct bag state", () => {
+        game.load(game002);
+
+        const tilesLeft = game.status().bag.length;
+
+        console.assert(tilesLeft === 0);
+    });
+
+    it("loads the correct board state", () => {
+        game.load(game002);
+
+        const board = parseBoard(`
+   A B C D E F G H I J K L M N O
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 1|"| | |.|T| | |"| | | |G|U|N|K|0
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 2| |'| | |U|:| | | |R| |O| |'| |1
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 3| | |'| |L| |.| |.|O| |O|R|C|A|2
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 4|.| | |'|I| | |J|A|D|E|S| | |.|3
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 5| |G|U|L|P| |Q|U|E|E|R| | | | |4
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 6| |O| | | |:| |V| |N| | | |:| |5
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 7| |O|Y| | | |.|I|.|T| | |.| | |6
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 8|"|D|E|C|L|I|N|e| | | |.|B|O|Y|7
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ 9| | |A| |I| |.| |N| |F|A|I|L| |8
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+10| |:| | |M|:| |W|A|Z|E|D| |:| |9
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+11| |B|E|E|P| |H|A|D| |W|E|E|S| |10
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+12|S|O|F|T| | |A|X|E| | |'| |H|.|11
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+13| |N|'| |V|I|N| |.| | | |'|O| |12
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+14| |E| | | |:|G| | |:| | | |o| |13
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+15|A|R|R|E|A|R|S|"| | | |T|I|T|I|14
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4`);
+
+        console.assert(_.isEqual(board, game.board()));
+    });
+
+    it("loads the correct team turn", () => {
+        game.load(game002);
+
+        console.assert(game.status().teamTurn === 1);
+    });
+
+    it("loads the correct racks", () => {
+        game.load(game002);
+
+        const racks = [["I", "M", "T"], []];
+
+        console.assert(_.isEqual(racks, game.status().racks));
+    });
+
+    it("loads the correct scores", () => {
+        game.load(game002);
+
+        const scores = [281, 336];
 
         console.assert(_.isEqual(scores, game.status().scores));
     });
